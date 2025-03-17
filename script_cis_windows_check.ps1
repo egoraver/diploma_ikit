6e97fc4beebe4315cc7b5d2b1f52cee2183ffc64
Egor <averyaskin2001@gmail.com>
2025-03-17 17:09:12 +0700

    0.0.1.4

--- script_cis_windows_check.ps1
+++ script_cis_windows_check.ps1
@@ -10,11 +10,13 @@ $Date = Get-Date -U %d%m%Y
 
 $result_file = "audit" + $date + "-" + $Win +".txt"
 
-Write-Host 'Начало проверки'
+Write-Host 'Start checkout'
 
 #Проверка парольной политики
 
-$recomendation = "1.1.1 (L1) Параметр 'Журнал паролей' установлен на 24 или более сохраненных паролей» (автоматически)" + ";"
+#$recomendation = "1.1.1 (L1) Параметр 'Журнал паролей' установлен на 24 или более сохраненных паролей» (автоматически)" + ";"
+$recomendation = "1.1.1 (L1) Ensure 'Enforce password history' is set to '24 or more
+password(s)' (Automated)" + ";" + `n
 $result = Get-Content ./secpool.cfg | Select-String "PasswordHistorySize" | select-object -First 1
 
 $recomendation += $result
