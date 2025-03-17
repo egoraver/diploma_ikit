91909ebaf6c7ed12249997422f02600b64a7a307
Egor <averyaskin2001@gmail.com>
2025-03-17 16:41:44 +0700

    0.0.1

---
+++ script_cis_windows_check.ps1
@@ -0,0 +1,15 @@
+#Requires -RunAsAdministrator
+
+secedit /export /cfg ./secpool.cfg
+
+Write-Host 'Начало проверки'
+
+#Проверка парольной политики
+
+$recomendation = "1.1.1 (L1) Параметр 'Журнал паролей' установлен на 24 или более сохраненных паролей» (автоматически)" + ";"
+$result = Get-Content ./secpool.cfg | Select-String "PasswordHistorySize" | select-object -First 1
+
+$recomendation += $result
+
+Clean-Value($recomendation)
+Clean-Value($result)
